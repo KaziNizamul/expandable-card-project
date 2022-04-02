9ed5a7f343bb11330f1c65dcf368a0839b6400bd
Kazi Nizamul <kazinizamul.work@hotmail.com>
2022-04-02 22:39:06 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,40 @@
-import { motion } from "framer-motion";
-import { useState } from "react";
+import { motion } from 'framer-motion';
+import { useState } from 'react';
 
-import "./styles.scss";
+import './styles.scss';
 
 function App() {
-  const [isOpen, setIsOpen] = useState(false);
-  return (
-    <div className="App">
-      <motion.div Layout onClick={() => setIsOpen(!isOpen)} className="card">
-        <motion.h2 Layout="position"> Framer motion </motion.h2>
-        {isOpen && (
-          <motion.div>
-            <p>
-              Lorem Ipsum is simply dummy text of the printing and typesetting
-              industry.
-            </p>
-            <p>scrambled it to make a type specimen book.</p>
-          </motion.div>
-        )}
-      </motion.div>
-    </div>
-  );
+	const [isOpen, setIsOpen] = useState(false);
+	return (
+		<div className="App">
+			<motion.div
+				layout
+				transition={{ layout: { duration: 1, type: 'spring' } }}
+				onClick={() => setIsOpen(!isOpen)}
+				className="card"
+				style={{
+					borderRadius: '1rem',
+					boxShadow: '0px 10px 30px rgba(0,0,0,0.5',
+				}}
+			>
+				<motion.h2 layout="position"> Framer motion 🚀</motion.h2>
+				{isOpen && (
+					<motion.div
+						initial={{ opacity: 0 }}
+						animate={{ opacity: 1 }}
+						transition={{ duration: 1 }}
+						className="expand"
+					>
+						<p>
+							Lorem Ipsum is simply dummy text of the printing and
+							typesetting industry.
+						</p>
+						<p>scrambled it to make a type specimen book.</p>
+					</motion.div>
+				)}
+			</motion.div>
+		</div>
+	);
 }
 
 export default App;
